e83b5016a14272d2cee7cf1dc8e9671f32761ff1
Prachi <prachijain@Prachis-MacBook-Air.local>
2021-07-29 01:30:45 +0530

    changes

--- server.js
+++ server.js
@@ -134,8 +134,8 @@ app.put('/image',(req,res)=>{
     
 })
 
-app.listen(3001,()=>{
-    console.log("App is running on port 3001")
+app.listen(process.env.PORT || 3001,()=>{
+    console.log(`App is running on port ${process.env.PORT}`)
 })
 
 
